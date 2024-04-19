bfb9b6ecac2e51abbf0072003713a29b4367e37c
Mads <mads.engberg@hotmail.com>
2024-04-19 20:02:04 +0200

    Init commit

---
+++ Reflection/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private PolygonCollider2D collider2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
